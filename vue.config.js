7d6e734e127b17bba2802efa892ba18074f888ac
chinafitz <498289134@qq.com>
2021-12-01 16:25:26 +0800

    <网红好物商城> 项目结构搭建完毕

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    productionSourceMap: false,
+    // 关闭ESLINT校验工具
+    lintOnSave: false,
+    //配置代理跨域
+    devServer: {
+        // 自动打开浏览器
+        open: true,
+    },
+};
