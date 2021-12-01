826dd28c124a5493758cff9b6b9f4ffd25269f79
chinafitz <498289134@qq.com>
2021-12-01 17:27:35 +0800

    修复 Avoided redundant navigation... 报错

--- src/main.js
+++ src/main.js
@@ -5,6 +5,26 @@ import store from './store'
 
 Vue.config.productionTip = false
 
+// ================================ 修复 Avoided redundant navigation... 报错 ==================================
+const originPush = Vue.prototype.push
+const originReplace = Vue.prototype.replace
+
+Vue.prototype.push = function(route, onFulfilled, onRejected) {
+    if (onFulfilled && onRejected) {
+        originPush.call(this, route, onFulfilled, onRejected)
+    }else {
+        originPush.call(this, route, ()=>{}, ()=>{})
+    }
+}
+Vue.prototype.replace = function(route, onFulfilled, onRejected) {
+    if (onFulfilled && onRejected) {
+        originReplace.call(this, route, onFulfilled, onRejected)
+    }else {
+        originReplace.call(this, route, ()=>{}, ()=>{})
+    }
+}
+// ================================ 修复 Avoided redundant navigation... 报错 ==================================
+
 new Vue({
   router,
   store,
